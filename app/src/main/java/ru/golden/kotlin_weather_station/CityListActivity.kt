075f250c8f2b1dd2f7dc59104aadb7987e3b0b0e
Matthew Khadzhiev <khad-mat123@yandex.ru>
2018-05-06 16:55:38 +0700

    Project created. Added some libraries

---
+++ app/src/main/java/ru/golden/kotlin_weather_station/CityListActivity.kt
@@ -0,0 +1,12 @@
+package ru.golden.kotlin_weather_station
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CityListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_list)
+    }
+}
